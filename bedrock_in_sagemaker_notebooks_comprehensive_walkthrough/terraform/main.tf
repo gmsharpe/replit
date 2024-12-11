0a0dbbc627b37e70da1793109ab5e4bddec1ad8e
gmsharpe <gmsharpe@ucdavis.edu>
2024-12-11 14:16:30 -0800

    no message

--- bedrock_in_sagemaker_notebooks_comprehensive_walkthrough/terraform/main.tf
+++ bedrock_in_sagemaker_notebooks_comprehensive_walkthrough/terraform/main.tf
@@ -1,12 +1,4 @@
-provider "aws" { region = "us-west-1" }
-
-resource "aws_s3_bucket" "sagemaker_bucket" {
-  bucket = "my-sagemaker-notebook-bucket-${random_string.suffix.id}"
-  acl    = "private"
-  versioning {
-    enabled = true
-  }
-}
+provider "aws" { region = "us-west-2" }
 
 resource "random_string" "suffix" {
   length  = 6
@@ -35,36 +27,40 @@ resource "aws_iam_role_policy_attachment" "sagemaker_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
 }
 
+resource "aws_iam_policy" "bedrock_invoke_policy" {
+  name        = "BedrockInvokePolicy"
+  description = "Policy to allow invoking Bedrock models"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = [
+          "bedrock:InvokeModel",
+          "bedrock:InvokeModelWithResponseStream"
+        ],
+        Resource = "arn:aws:bedrock:*::foundation-model/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_bedrock_invoke_policy" {
+  role       = aws_iam_role.sagemaker_execution_role.name
+  policy_arn = aws_iam_policy.bedrock_invoke_policy.arn
+}
+
 resource "aws_sagemaker_notebook_instance" "sagemaker_notebook" {
   name              = "sagemaker-notebook-${random_string.suffix.id}"
-  instance_type     = "ml.t2.medium"
+  instance_type     = "ml.t3.medium"
   role_arn          = aws_iam_role.sagemaker_execution_role.arn
 
-  lifecycle_config_name = aws_sagemaker_notebook_instance_lifecycle_configuration.lifecycle_config.name
-}
-
-resource "aws_sagemaker_notebook_instance_lifecycle_configuration" "lifecycle_config" {
-  name = "sagemaker-notebook-lifecycle-config"
-
-  on_create = <<EOT
-#!/bin/bash
-set -e
-
-# Custom initialization logic can go here
-EOT
-
-  on_start = <<EOT
-#!/bin/bash
-set -e
-
-# Custom startup logic can go here
-EOT
-}
-
-output "sagemaker_bucket_name" {
-  value = aws_s3_bucket.sagemaker_bucket.bucket
 }
 
 output "sagemaker_notebook_name" {
   value = aws_sagemaker_notebook_instance.sagemaker_notebook.name
 }
+
+# output "sagemaker_notebook_url" {
+#   value = aws_sagemaker_notebook_instance.sagemaker_notebook.url
+# }
